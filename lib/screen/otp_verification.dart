325c2b96ec32a1f971915b8a1711a73348e16a08
ogomegbunam <58264605+ogomegbunam@users.noreply.github.com>
2022-01-07 17:56:30 +0100

    update

--- lib/screen/otp_verification.dart
+++ lib/screen/otp_verification.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:handshake/components/reusable_button.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 import '../constants.dart';
 
-class OtpVerification extends StatelessWidget {
+class OtpVerification extends StatefulWidget {
   const OtpVerification({Key? key}) : super(key: key);
 
+  @override
+  State<OtpVerification> createState() => _OtpVerificationState();
+}
+
+class _OtpVerificationState extends State<OtpVerification> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,27 +75,92 @@ class OtpVerification extends StatelessWidget {
               SizedBox(
                 height: 30,
               ),
-              Text(
-                'What\'s my number?',
-                style: kblkheading,
+            PinCodeTextField(
+              appContext: context,
+              pastedTextStyle: TextStyle(
+                color: inputColor,
+                fontWeight: FontWeight.bold,
               ),
+              length: 5,
+              obscureText: true,
+              obscuringCharacter: '*',
+
+              blinkWhenObscuring: true,
+              animationType: AnimationType.fade,
+              validator: (v) {
+                if (v!.length < 4) {
+                  return "I'm from validator";
+                } else {
+                  return null;
+                }
+              },
+              enablePinAutofill: true,
+
+              pinTheme: PinTheme(
+                inactiveColor: inputColor,
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(50),
+                selectedFillColor: inputColor,
+                errorBorderColor: inputColor,
+                fieldHeight: 50,
+                fieldWidth: 50,
+                activeFillColor:inputColor,
+                inactiveFillColor: inputColor,
+                activeColor: inputColor,
+                disabledColor: inputColor,
+              ),
+              cursorColor: Colors.black,
+              animationDuration: Duration(milliseconds: 300),
+              enableActiveFill: true,
+
+             // errorAnimationController: errorController,
+              //controller: textEditingController,
+              keyboardType: TextInputType.number,
+              boxShadows: [
+                BoxShadow(
+                  offset: Offset(0, 1),
+                  color: Colors.black12,
+                  blurRadius: 10,
+                )
+              ],
+              onCompleted: (v) {
+                print("Completed");
+              },
+              // onTap: () {
+              //   print("Pressed");
+              // },
+              onChanged: (value) {
+                print(value);
+                setState(() {
+                 // currentText = value;
+                });
+              },
+              beforeTextPaste: (text) {
+                print("Allowing to paste $text");
+                //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                return true;
+              },
+            ),
+
+
               SizedBox(
                 height: 50,
               ),
               OtherButton(onpressedfunction: () {}, label: 'Verify'),
               SizedBox(height: 40,),
-              Center(
-                  child: Row(
-                    children: [
-                      Center(
-                        child: Text(
-                          'Didn\'t receive code? ',
-                          style: ksubheading,
-                        ),
-                      ),
-                      Text('Resend', style: kblkheading)
-                    ],
-                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      'Didn\'t receive code? ',
+                      style: ksubheading,
+                    ),
+                  ),
+                  Text('Resend', style: kblkheading)
+                ],
+              ),
             ],
           ),
         ),
